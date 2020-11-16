6ac5027f434f540f1643a0dd59408bfd0fd0f0a7
farewell12345 <1149558764@qq.com>
2020-11-16 13:17:53 +0800

    兼容多种格式的yml

--- src/main/java/io/farewell12345/github/faqbot/AppConfig.java
+++ src/main/java/io/farewell12345/github/faqbot/AppConfig.java
@@ -5,7 +5,11 @@ import org.yaml.snakeyaml.Yaml;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Timer;
 
 import static io.farewell12345.github.faqbot.curd.DBUntilKt.logger;
 
@@ -17,18 +21,20 @@ public class AppConfig {
     public Long BotQQ;
     public String BotPwd;
     public Long SuperUser;
+    public ArrayList<String> times;
     private AppConfig() throws FileNotFoundException {
         Yaml yml = new Yaml();
-        Map<String,String> data = yml.load(new FileReader( new File("config.yml")));
-        dbUrl=data.get("dbUrl");
+        Map<String, Object> data = yml.load(new FileReader( new File("config.yml")));
+        dbUrl= (String) data.get("dbUrl");
         if (dbUrl==null){
             dbUrl = "jdbc:mysql://localhost:3306/fqa?serverTimezone=UTC&characterEncoding=UTF-8";
         }
-        dbUser=data.get("dbUser");
-        dbPwd=data.get("dbPwd");
-        SuperUser = Long.valueOf(data.get("superUser"));
-        BotQQ= Long.valueOf(data.get("botQQ"));
-        BotPwd=data.get("botPwd");
+        dbUser= (String) data.get("dbUser");
+        dbPwd= (String) data.get("dbPwd");
+        SuperUser = Long.valueOf((String) data.get("superUser"));
+        BotQQ= Long.valueOf((String) data.get("botQQ"));
+        BotPwd= (String) data.get("botPwd");
+        times = (ArrayList<String>) data.get("time");
         logger().info("配置加载完成！"+dbUrl);
     }
 
@@ -39,4 +45,12 @@ public class AppConfig {
         return INSTANCE;
     }
 }
-
+//class main {
+//    public static void main(String[] args) {
+//        try {
+//            AppConfig.getInstance();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}

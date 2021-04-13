6dd687d259105ea63204495f7f2214bef2539f44
Umb <1149558764@qq.com>
2021-04-13 21:57:35 +0800

    lolicon api

--- src/main/java/io/farewell12345/github/faqbot/AppConfig.java
+++ src/main/java/io/farewell12345/github/faqbot/AppConfig.java
@@ -23,9 +23,11 @@ public class AppConfig {
     public String[] disRepetitionScence = new String[]{"复 读 禁 止", "禁 止 复 读"};
     public String[] badOrGood = {"凶", "吉"};
     public String[] draws = {"大", "中", "小"};
-    public String getStudentUrl(String ID){
-        return "http://10.101.163.66:2799/sign?id="+ID;
+
+    public String getStudentUrl(String ID) {
+        return "http://10.101.163.66:2799/sign?id=" + ID;
     }
+
     private AppConfig() throws FileNotFoundException {
         Yaml yml = new Yaml();
         Map<String, Object> data = yml.load(new FileReader(new File("config.yml")));
@@ -33,14 +35,20 @@ public class AppConfig {
         if (dbUrl == null) {
             throw new NullPointerException("请在config.yml文件中正确配置jdbc数据库url");
         }
-        gameDBUrl = (String) data.get("gameDBUrl");
-        gameAPI = (String) data.get("GameAPI");
+
         dbUser = (String) data.get("dbUser");
         dbPwd = (String) data.get("dbPwd");
-        superUser = Long.valueOf((String) data.get("superUser"));
         botQQ = Long.valueOf((String) data.get("botQQ"));
         botPwd = (String) data.get("botPwd");
-        loliconKey = (String) data.get("lolicon");
+        try {
+            superUser = Long.valueOf((String) data.get("superUser"));
+            loliconKey = (String) data.get("lolicon");
+            gameDBUrl = (String) data.get("gameDBUrl");
+            gameAPI = (String) data.get("GameAPI");
+        } catch (Exception ignored) {
+            logger().info("缺失gameDBUrl，gameAPI、loliconKey可能导致Bot某些功能无法正常使用");
+        }
+
         logger().info("配置加载完成！" + dbUrl);
     }
 

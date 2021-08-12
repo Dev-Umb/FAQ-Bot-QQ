070ce42595a07608689010726098b511d3014741
Umb <1149558764@qq.com>
2021-08-12 22:33:23 +0800

    新增：error重启

---
+++ src/main/kotlin/io/farewell12345/github/faqbot/Plugin/ProgramManage.kt
@@ -0,0 +1,26 @@
+package io.farewell12345.github.faqbot.Plugin
+
+import io.farewell12345.github.faqbot.BotManager.BotsManager
+import io.farewell12345.github.faqbot.BotManager.CommandGroupList
+import io.farewell12345.github.faqbot.DTO.model.logger
+import io.farewell12345.github.faqbot.Listener.BaseListeners
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.joinAll
+import net.mamoe.mirai.alsoLogin
+
+object ProgramManage {
+    suspend fun run(){
+        val bot = BotsManager.loginBot()  //登录bot
+        BaseListeners.listeners.forEach {
+            bot.eventChannel.registerListenerHost(it)
+        }
+    //    PicManager
+        CommandGroupList.init()
+        val logger = logger() // 打印日志
+        joinAll(BotsManager.jobs) // 将BotsManager的监听事件加入协程
+    }
+    suspend fun restart(){
+        BotsManager.oneBot?.bot?.close()
+        run()
+    }
+}

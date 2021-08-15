ef134d4210cc5a0bf4150fe5a2c97e707666edf7
farewell12345 <1149558764@qq.com>
2021-08-15 15:49:37 +0800

    SQL

--- SQL/FQADB.sql
+++ SQL/FQADB.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : tencentSQL
+ Source Server         : FAQDB
  Source Server Type    : MySQL
  Source Server Version : 50718
  Source Host           : bj-cynosdbmysql-grp-kf9vc2hk.sql.tencentcdb.com:21437
@@ -11,7 +11,7 @@
  Target Server Version : 50718
  File Encoding         : 65001
 
- Date: 12/08/2021 02:19:19
+ Date: 15/08/2021 15:47:22
 */
 
 SET NAMES utf8mb4;
@@ -30,29 +30,35 @@ CREATE TABLE `group`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Table structure for manager
+-- ----------------------------
+DROP TABLE IF EXISTS `manager`;
+CREATE TABLE `manager`  (
+  `id` int(11) NOT NULL,
+  `qq` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
 -- Table structure for message
 -- ----------------------------
 DROP TABLE IF EXISTS `message`;
 CREATE TABLE `message`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `data` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
   `question_id` int(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 250 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for pic
+-- Table structure for notice
 -- ----------------------------
-DROP TABLE IF EXISTS `pic`;
-CREATE TABLE `pic`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pic` longblob NOT NULL,
-  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `pvid` bigint(20) NOT NULL,
-  `time` datetime(0) NULL DEFAULT NULL,
+DROP TABLE IF EXISTS `notice`;
+CREATE TABLE `notice`  (
+  `id` int(11) NOT NULL,
+  `message_id` int(11) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for question
@@ -62,35 +68,14 @@ CREATE TABLE `question`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `answer_id` int(11) NULL DEFAULT NULL,
   `question_id` int(11) NULL DEFAULT NULL,
-  `group` int(20) NULL DEFAULT NULL,
-  `last_edit_user` int(20) NULL DEFAULT NULL,
+  `group` bigint(20) NULL DEFAULT NULL,
+  `last_edit_user` bigint(100) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `FKhmw49oeaxy93yc72okw2voxjk`(`answer_id`) USING BTREE,
   INDEX `FK6tbw2ts0kacdf64f5b2hansqy`(`question_id`) USING BTREE,
   CONSTRAINT `FK6tbw2ts0kacdf64f5b2hansqy` FOREIGN KEY (`question_id`) REFERENCES `message` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `FKhmw49oeaxy93yc72okw2voxjk` FOREIGN KEY (`answer_id`) REFERENCES `message` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for remote
--- ----------------------------
-DROP TABLE IF EXISTS `remote`;
-CREATE TABLE `remote`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tag_id` int(11) NOT NULL,
-  `pic_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for tag
--- ----------------------------
-DROP TABLE IF EXISTS `tag`;
-CREATE TABLE `tag`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 331 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 122 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for welcome
@@ -101,6 +86,6 @@ CREATE TABLE `welcome`  (
   `group` bigint(255) NOT NULL,
   `talk` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;

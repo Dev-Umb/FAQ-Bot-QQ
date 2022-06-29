ec42fd95dac80c8a3e84477e616656910422169b
farewell12345 <1149558764@qq.com>
2022-06-30 01:57:03 +0800

    让它能跑

--- src/main/kotlin/umb/ink/ktor/Application.kt
+++ src/main/kotlin/umb/ink/ktor/Application.kt
@@ -5,11 +5,9 @@ import io.ktor.server.netty.*
 import umb.ink.ktor.bot.QABot
 import umb.ink.ktor.plugins.*
 import umb.ink.ktor.config.configureHTTP
-import umb.ink.ktor.routing.configureRouting
 
 suspend fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
         configureSecurity()
         configureHTTP()
         QABot.login()
--- src/main/kotlin/umb/ink/ktor/plugins/Serialization.kt
+++ src/main/kotlin/umb/ink/ktor/plugins/Serialization.kt
@@ -1,11 +1,9 @@
 package umb.ink.ktor.plugins
 
 import io.ktor.serialization.gson.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
 
 fun Application.configureSerialization() {
@@ -16,11 +14,6 @@ fun Application.configureSerialization() {
     }
 
     routing {
-        get("/json/gson") {
-            call.respond(mapOf("hello" to "world"))
-        }
-        get("/json/kotlinx-serialization") {
-            call.respond(mapOf("hello" to "world"))
-        }
+
     }
 }
